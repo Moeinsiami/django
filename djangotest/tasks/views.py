3f51b7f285544ee74d22b70dc0e9e30b48f4ca09
moeinsiami <moeins1216@gmail.com>
2022-12-08 16:10:13 +0330

    .

--- djangotest/tasks/views.py
+++ djangotest/tasks/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+tasks = ['check mail', 'send gift', 'shop']
 # Create your views here.
 def index(request):
-    return render(request, 'tasks/index.html')
+    return render(request, 'tasks/index.html',{
+        "tasks": tasks
+    })
